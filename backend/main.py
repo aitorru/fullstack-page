6e467a61f9a5af9275c05e17a085a66122198725
Aitor Ruiz <aitorruiz2m@gmail.com>
2021-10-17 19:36:10 +0200

    :construction: Mongo db connection

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from pymongo import MongoClient
 import sys
 
 app = Flask(__name__)
 CORS(app)
+client = None
+if len(sys.argv) > 1 and sys.argv[1] == "True":
+    # In "production" take the docker internal connection
+    # TODO: Get credentials from root file
+    client = MongoClient("mongodb://root:example@db:27017/")
+else:
+    # TODO: Get credentials from root file
+    client = MongoClient("mongodb://root:example@localhost:27017")
 
 
 @app.route("/api/hello")
