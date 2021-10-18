6f59bf6e27519f978c1af3a5a0d5dd6e454a7fc9
Aitor Ruiz <aitorruiz2m@gmail.com>
2021-10-18 18:08:19 +0200

    :lipstick: Grid layout start

--- frontend/src/Components/news/index.js
+++ frontend/src/Components/news/index.js
@@ -1,8 +1,13 @@
 export default function News() {
     return (
         <div className="w-screen md:container md:mx-auto">
-            <div className='grid grid-cols-2 w-full'>
+            <div className='md:m-8 grid grid-cols-1 md:grid-cols-3 w-full gap-8'>
+                <div className="rounded-xl md:col-span-2 h-30 md:h-72 w-full bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
 
+                </div>
+                <div className="rounded-xl md:col-span-1 h-60 w-full bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+
+                </div>
             </div>
         </div>
     )
